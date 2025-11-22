86e0d574f44742334c9a20470e1d39ad6f382648
Enes <enesgurden@hotmail.com>
2025-11-22 19:42:24 +0300

    function pointer

--- main.cpp
+++ main.cpp
@@ -4,37 +4,29 @@
 using namespace std;
 using namespace fmt;
 
-#define STRINGCONTROL 0
-// #define CHARCONTROL 1
-// #define INTCONTROL 2
+double ali(int n)
+{
+    return n * 0.05;
+}
+
+double ayse(int n)
+{
+    return 0.03 * n + n * n * 0.0062;
+}
+
+void estimate(int lines, double (*pf)(int))
+{
+    println("{} lines of code will take {} hours", lines, (*pf)(lines));
+}
 
 int main()
 {
-#ifdef STRINGCONTROL
-    string str;
-    println("enter a string");
-    while (getline(cin, str) && str != "q") {
-        println("this is a string(q to quit)");
-    }
-#endif
-
-#ifdef CHARCONTROL
-    char ch;
-    println("enter a char");
-
-    while (cin >> ch && ch != 'q') {
-        println("this is a char(q to quit)");
-    }
-
-#endif
-
-#ifdef INTCONTROL
-    int i;
-    println("enter an int");
-    while (cin >> i) {
-        println("this is an int(press anything but not a number to quit)");
-    }
-#endif
-    println("program is over");
+    int lines;
+    println("How many lines of code you will write?");
+    cin >> lines;
+    println("Ali's estimation is:");
+    estimate(lines, ali);
+    println("Ayse's estimation is:");
+    estimate(lines, ayse);
     return 0;
 }
