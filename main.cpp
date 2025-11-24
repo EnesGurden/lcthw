8d2ea7ac1f839a350c0969083be0774e7ee09f07
U-MUKAAHSAP\Yazilim <enesgurden@hotmail.com>
2025-11-24 14:00:35 +0300

    function overloading

--- main.cpp
+++ main.cpp
@@ -1,83 +1,50 @@
-#include <fmt/core.h>
 #include <iostream>
 
-using namespace fmt;
-using namespace std;
-
-#define REFERENCEVARIABLE 0;
-// #define REFERENCEFUNCTION 1;
-// #define REFERENCESTRUCT 2;
-// #define REFERENCECLASS 3;
+int left(int num, int ct);
+char* left(const char* str, int n = 1); // 1 is a default argument of Argument 2
 
 int main()
 {
-#ifdef REFERENCEVARIABLE
-    int foo = 62;
-    int& rv = foo;
-    println("foo: {}", foo);
-    rv++;
-    println("foo: {}", foo);
-    println("foo add: {}, rv add: {}", (void*)&foo, (void*)&rv);
-    cout << "foo add: " << &foo << ", rv add: " << &rv << endl;
-#endif
-#ifdef REFERENCEFUNCTION
-    auto swapref_lambda = [](int& a, int& b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    };
-    int v1 = 62;
-    int v2 = 6262;
-    println("initial v1: {}, v2: {}", v1, v2);
-    swapref_lambda(v1, v2);
-    println("final v1: {}, v2: {}", v1, v2);
-#endif
-#ifdef REFERENCESTRUCT
-    struct free_throws {
-        std::string name;
-        int made;
-        int attempts;
-        float percent;
-    };
-    auto display = [](const free_throws& rs) {
-        println("name: {}, made: {}, attempts: {}, percent: {}", rs.name, rs.made, rs.attempts, rs.percent);
-    };
-    auto accumulate = [](free_throws& target, const free_throws& source) {
-        target.percent += source.made;
-        return target;
-    };
-
-    free_throws one = { "Ifelsa Branch", 13, 14 };
-    free_throws two = { "Andor Knott", 10, 16 };
-    free_throws three = { "Minnie Max", 7, 9 };
-    free_throws team = { "Throwgoods", 0, 0 };
-    display(team);
-    display(accumulate(team, one));
-    free_throws four = accumulate(team, two);
-    display(four);
-    display(accumulate(team, three));
-#endif
-#ifdef REFERENCECLASS
-    auto version1 = [](const string& s1, const string& s2) {
-        string temp;
-        temp = s2 + s1 + s2;
-        return temp;
-    };
-    auto version2 = [](string& s1, const string& s2) {
-        s1 = s2 + s1 + s2;
-        return s1;
-    };
-    string input;
-    string result;
-    cout << "Enter a string: ";
-    getline(cin, input);
-    cout << "Your string as entered: " << input << endl;
-    result = version1(input, "***");
-    cout << "Your string enhanced: " << result << endl;
-    cout << "Your original string: " << input << endl;
-    result = version2(input, "###");
-    cout << "Your string enhanced: " << result << endl;
-    cout << "Your original string: " << input << endl;
-#endif
+    using namespace std;
+    const char* trip = "Hawaii!!";
+    const int n = 12345678;
+    int i;
+    char* temp;
+    for (i = 1; i < 10; i++) {
+        cout << left(n, i) << endl;
+        temp = left(trip, i);
+        cout << temp << endl;
+        delete[] temp; // point to temporary storage
+    }
     return 0;
+}
+// This function returns the first ct digits of the number num.
+int left(int num, int ct)
+{
+    int digits = 1;
+    int long n = num;
+    if (ct == 0 || num == 0)
+        return 0;
+    while (n /= 10)
+        digits++;
+    if (digits > ct) {
+        ct = digits - ct;
+        while (ct--)
+            num /= 10;
+        return num;
+    } else
+        return num;
+}
+// This function returns a pointer to a new string consisting of the first n characters in the str string.
+char* left(const char* str, int n)
+{
+    if (n < 0)
+        n = 0;
+    char* p = new char[n + 1];
+    int i;
+    for (i = 0; i < n && str[i]; i++)
+        p[i] = str[i]; // copy characters
+    while (i <= n)
+        p[i++] = '\0'; // set rest of string to null
+    return p;
 }
